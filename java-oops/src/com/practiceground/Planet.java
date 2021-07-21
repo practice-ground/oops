0030b0d5af2947ad328cb798cd57183f4a077e19
practice-ground <practiceground21@gmail.com>
2021-07-21 13:30:37 +0530

    inital import

---
+++ java-oops/src/com/practiceground/Planet.java
@@ -0,0 +1,12 @@
+package com.practiceground;
+
+public class Planet {
+	
+	Planet()
+	{
+		
+	}
+	public void rotate() {
+		System.out.println("Rotating!!!");
+	}
+}
---
+++ java-oops/src/com/practiceground/PlanetTest.java
@@ -0,0 +1,10 @@
+package com.practiceground;
+
+public class PlanetTest {
+	
+	public static void main(String args[]) {
+		Planet earth  = new Planet();
+		earth.rotate();
+	}
+
+}
